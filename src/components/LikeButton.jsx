98f8528881af62eaf11ebd6b5190d18ecacb2a3e
yovel-sys <yovel777@gmail.com>
2025-09-20 23:12:45 +0300

    deleted for pull request

--- src/components/LikeButton.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function LikeButton() {
-  const [isliked, setIsLiked] = useState(false);
-  function handleLike() {
-    setIsLiked((like) => !like);
-  }
-  return <button onClick={handleLike}>{isliked ? "❤️" : "♡"}</button>;
-}
