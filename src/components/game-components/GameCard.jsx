48da0edf4e0edc2117e396e13b824cd00b9bc446
yovel-sys <yovel777@gmail.com>
2025-11-01 15:58:23 +0200

    1

--- src/components/game-components/GameCard.jsx
+++ src/components/game-components/GameCard.jsx
@@ -8,14 +8,12 @@ export default function GameCard({ handleLike, game, className, openModal }) {
       className={`h-90 w-100 !bg-gray-400 !opacity-90 border-4 p-3 pb-10 m-2 rounded-2xl ${className}`}
       onClick={() => openModal(game)}
     >
-      <div>
-        <div className="flex items-start flex-grow mb-4">
-          <img src={gameImagePlaceolder} alt="" className="h-25 w-20 mr-4" />
-          <div>
-            <h1 className="m-1">{game.gameName}</h1>
-            <p className="m-1">Released: {game.releaseDate}</p>
-            <p>{game.gameDescription}</p>
-          </div>
+      <div className="flex items-start flex-grow mb-4">
+        <img src={gameImagePlaceolder} alt="" className="h-25 w-20 mr-4" />
+        <div>
+          <h1 className="m-1">{game.gameName}</h1>
+          <p className="m-1">Released: {game.releaseDate}</p>
+          <p>{game.gameDescription}</p>
         </div>
       </div>
       <LikeButton
--- src/hooks/useModal.jsx
+++
@@ -1,7 +0,0 @@
-import { useState, useCallback } from "react";
-export default function useModal() {
-  const [open, setOpen] = useState(false);
-  const openModal = useCallback(() => setOpen(true), []);
-  const closeModal = useCallback(() => setOpen(false), []);
-  return open, openModal, closeModal, setOpen;
-}
